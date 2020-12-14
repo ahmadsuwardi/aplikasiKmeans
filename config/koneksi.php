a8ed1c238901809a2c15dc5e30e6a2ba838148a9
unknown <ahmadsuwardi.1711010173@mail.darmajaya.ac.id>
2020-12-14 16:23:06 +0700

    tamnah file

---
+++ config/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "dmkmeanv1";
+
+mysqli_connect($server,$username,$password,$database) or die ("gagal, Database tidak ditemukan");
+?>
